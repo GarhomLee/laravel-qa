f61fa6e2ae5eaea1aaef9fbf4de62d7801fadeae
JIAHONG LI <jiahongli@JIAHONGs-MacBook-Pro.local>
2019-09-08 20:43:52 -0700

    Implement many to many relationship and make the favorite button works

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -26,10 +26,19 @@
                             <a title="This question is not useful" class="vote-down off" href="">
                                 <i class="fa fa-caret-down fa-3x"></i>
                             </a>
-                            <a title="Click to mark as favorite question (Click again to undo)" class="favorite mt-2 favorited" href="">
+                            <a title="Click to mark as favorite question (Click again to undo)" 
+                                class="favorite mt-2 {{ Auth::guest() ? 'off' : ($question->is_favorited ? 'favorited' : '') }}"
+                                onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit();"
+                                >
                                 <i class="fa fa-star fa-2x"></i>
-                                <span class="favorites_count">2</span>
+                                <span class="favorites-count">{{ $question->favorites_count }}</span>
                             </a>
+                            <form id="favorite-question-{{ $question->id }}" action="/questions/{{ $question->id }}/favorites" method="post" style="display:none;">
+                                    @csrf
+                                    @if ($question->is_favorited)
+                                        @method('DELETE')
+                                    @endif
+                                </form>
                         </div>
 
                         <div class="media-body">

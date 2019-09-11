cafa6960ee8d9eee62304fa626705b8f85a1ff1c
JIAHONG LI <jiahongli@JIAHONGs-MacBook-Pro.local>
2019-09-10 21:25:53 -0700

    Tidy up the views

--- resources/views/shared/_vote.blade.php
+++ resources/views/shared/_vote.blade.php
@@ -12,17 +12,16 @@
 
 @php
     $formId = $name . "-" . $model->id;
-    $formAction = "/{ $firstURISegment }/{ $model->id }/vote";
+    $formAction = "/{$firstURISegment}/{$model->id}/vote";
 @endphp
-
-<div class="d-flex flex-column vote-controls">
+<div class="d-fex flex-column vote-controls">
     <a title="This {{ $name }} is useful" 
         class="vote-up {{ Auth::guest() ? 'off' : '' }}"
         onclick="event.preventDefault(); document.getElementById('up-vote-{{ $formId }}').submit();"
         >
-        <i class="fa fa-caret-up fa-3x"></i>
+        <i class="fas fa-caret-up fa-3x"></i>
     </a>
-    <form id="up-vote-{{ $formId }}" action="{{ $formAction }}" method="post" style="display:none;">
+    <form id="up-vote-{{ $formId }}" action="{{ $formAction }}" method="POST" style="display:none;">
         @csrf
         <input type="hidden" name="vote" value="1">
     </form>
@@ -30,23 +29,23 @@
     <span class="votes-count">{{ $model->votes_count }}</span>
 
     <a title="This {{ $name }} is not useful" 
-        class="vote-down off {{ Auth::guest() ? 'off' : '' }}"
+        class="vote-down {{ Auth::guest() ? 'off' : '' }}"
         onclick="event.preventDefault(); document.getElementById('down-vote-{{ $formId }}').submit();"
-    >
-        <i class="fa fa-caret-down fa-3x"></i>
+        >
+        <i class="fas fa-caret-down fa-3x"></i>
     </a>
-    <form id="down-vote-{{ $formId }}" action="{{ $formAction }}" method="post" style="display:none;">
+    <form id="down-vote-{{ $formId }}" action="{{ $formAction }}" method="POST" style="display:none;">
         @csrf
         <input type="hidden" name="vote" value="-1">
     </form>
-
+    
     @if ($model instanceof App\Question)
         @include ('shared._favorite', [
-            'model' => $model    
+            'model' => $model
         ])
     @elseif ($model instanceof App\Answer)
         @include ('shared._accept', [
-            'model' => $model    
-        ]) 
+            'model' => $model
+        ])
     @endif
 </div>
